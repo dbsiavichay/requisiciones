a2dbca54a3bb92febcf648d45a2d9a6502a273b7
Denis Siavichay <dbsiavichay@gmail.com>
2017-11-14 17:17:18 -0500

    Administrador de inventario

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CategoriaAdmin(admin.ModelAdmin):
+	pass	
+
+class ProductoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'categoria')
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Producto, ProductoAdmin)
