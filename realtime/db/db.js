828ed3144ba28dd6605e0adcd999a94013d1f34a
Denis Siavichay <dbsiavichay@gmail.com>
2018-02-15 00:49:44 -0500

    Default para el campo fecha en  la tabla de chat

--- realtime/db/db.js
+++ realtime/db/db.js
@@ -8,7 +8,7 @@ const query = client.query(
 	'CREATE TABLE public.node_mensaje(' +
 	  	'id serial NOT NULL,' +
 	  	'mensaje text,' +
-	  	'fecha_creacion timestamp with time zone NOT NULL,' +
+	  	'fecha_creacion timestamp with time zone NOT NULL DEFAULT now(),' +
 	  	'receptor_id integer NOT NULL,' +
 	  	'remitente_id integer NOT NULL,' +
 	  	'CONSTRAINT node_mensaje_pkey PRIMARY KEY (id)' +
